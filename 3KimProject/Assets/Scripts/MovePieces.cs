01376efb32477b20484873220b925485ccf189dd
jeewook0427 <jeewook2@gmail.com>
2020-08-20 17:37:40 +0900

    퍼즐 작업중 part1 완료 part2 ing

---
+++ 3KimProject/Assets/Scripts/MovePieces.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePieces : MonoBehaviour
+{
+    public static MovePieces instance;
+    Match3 game;
+
+    NodePiece moving;
+    Point newIndex;
+    Vector2 mouseStart;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        game = GetComponent<Match3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moving !=null)
+        {
+            Vector2 dir = ((Vector2)Input.mousePosition - mouseStart);
+            Vector2 nDir = dir.normalized;
+            Vector2 aDir = new Vector2(Mathf.Abs(dir.x), Mathf.Abs(dir.y));
+
+            newIndex = Point.clone(moving.index);
+            Point add = Point.zero;
+        }
+    }
+}
---
+++ 3KimProject/Assets/Scripts/NodePiece.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class NodePiece : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public int value;
+    public Point index;
+
+    [HideInInspector]
+    public Vector2 pos;
+    [HideInInspector]
+    public RectTransform rect;
+
+    bool updating;
+    Image img;
+
+    public void Initalize(int v, Point p, Sprite piece)
+    {
+        img = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+
+        value = v;
+        SetIndex(p);
+        img.sprite = piece;
+    }
+
+    public void SetIndex(Point p)
+    {
+        index = p;
+        ResetPosition();
+        UpdateName();
+    }
+
+    public void ResetPosition()
+    {
+        pos = new Vector2(32 + (64 * index.x), -32 - (64 * index.y));
+    }
+
+    public bool UpdatePiece()
+    {
+        return true;
+    }
+    void UpdateName()
+    {
+        transform.name = "Node [" + index.x + ", " + index.y + "]";
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (updating) return;
+        Debug.Log("Grab" + transform.name);        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Let go" + transform.name);
+    }
+}
